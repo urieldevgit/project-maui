ae84fbe2a40a1869d50d6469196eafac31afec67
Uriel Díaz <uriel@underdog.gs>
2020-11-05 23:31:45 -0600

    adding is_active validation to get endpoint

--- services/PostService.js
+++ services/PostService.js
@@ -6,7 +6,11 @@ module.exports = {
   create: (body) => new Post(body),
   getPosts: () => Post.find({ is_active: true }),
   getPost: (idPost) => Post.findById(idPost),
-  getByIdInUser: (idPost, user) => user.posts.id(idPost),
+  getByIdInUser: (idPost, user) => {
+    const post = user.posts.id(idPost);
+    if (post.is_active === false) return undefined;
+    return post;
+  },
   updateByIdInUser: (idPost, user, body) => {
     // El map() itera cada elemento del arreglo
     const updatedPosts = user.posts.map((post) => {
